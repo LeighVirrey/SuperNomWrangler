fbab759f380229f627e1fe6f2c5ffa52247fd050
Madison Pantheon <madison@webdevbydesign.com>
2025-04-24 11:28:39 -0600

    Nav, and home started

--- front-end/nom-wrangler/src/App.js
+++ front-end/nom-wrangler/src/App.js
@@ -1,20 +1,50 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './component/home/home'
+import Map from './component/map/map'
+import Profile from './component/profile/profile'
+import Login from './component/login/login'
+import Nav from './component/nav/nav'
+
+
+
 
 function App() {
   return (
-    <nav>
-      <div className='nav'>
-        <img alt='logo' className='logo' src='../images/nomWrangler2.png'></img>
+    <Router>
+      <div>
+        <Nav/>
+        {/* <nav>
+          <div className='nav'>
+            <Link to="/">
+            <img alt='logo' className='logo' src='../images/nomWrangler2.png'></img>
+            </Link>
+            <div className='buttons'>
+              <Link to="/">
+                <button className="navbutton">HOME</button>
+              </Link>
+              <Link to="/map">
+                <button className="navbutton">MAP</button>
+              </Link>
+              <Link to="/profile">
+                <button className="navbutton">PROFILE</button>
+              </Link>
+              <Link to="/login">
+                <button className="navbutton">LOGIN</button>
+              </Link>
+            </div>
+          </div>
+        </nav> */}
 
-        <div className='buttons'>
-          <button className="navbutton">HOME</button>
-          <button className="navbutton">MAP</button>
-          <button className="navbutton">PROFILE</button>
-          <button className="navbutton">LOGIN</button>
-        </div>
-
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
-    </nav>
+    </Router>
   );
 }
 
