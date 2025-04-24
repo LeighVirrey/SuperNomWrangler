56330cdbffefb73a914e5db28fd7d9bb047df247
Madison Pantheon <madison@webdevbydesign.com>
2025-04-24 11:28:39 -0600

    Nav, and home started

--- front-end/nom-wrangler/src/component/home/home.js
+++ front-end/nom-wrangler/src/component/home/home.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './home.css'
 
-const home = () => {
+const Home = () => {
   return (
-    <div>home</div>
+    <div>
+      <h1 className='header'> FEATURED </h1>
+
+      <div className='rest'>
+        <div className='singleRest'> 
+            <div>
+              <h1>Lucky J Steakhouse & Arena</h1>
+            </div>
+        </div>
+      </div>
+      <h1 className='header'> TOP REVIEWERS </h1>
+
+    </div>
   )
 }
 
-export default home
+export default Home;
--- front-end/nom-wrangler/src/component/nav/nav.js
+++ front-end/nom-wrangler/src/component/nav/nav.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const nav = () => {
   return (
     <nav>
-      <div>
-        <button class="nav-button">Home</button>
-        <button class="nav-button">About</button>
-      </div>
-    </nav>
+          <div className='nav'>
+            <Link to="/">
+            <img alt='logo' className='logo' src='../images/nomWrangler2.png'></img>
+            </Link>
+            <div className='buttons'>
+              <Link to="/">
+                <button className="navbutton">HOME</button>
+              </Link>
+              <Link to="/map">
+                <button className="navbutton">MAP</button>
+              </Link>
+              <Link to="/profile">
+                <button className="navbutton">PROFILE</button>
+              </Link>
+              <Link to="/login">
+                <button className="navbutton">LOGIN</button>
+              </Link>
+            </div>
+          </div>
+        </nav>
   )
 }
+ export default nav;
