39387df968c09f784316538d93e137745daff25c
Madison Pantheon <madison@webdevbydesign.com>
2025-04-30 11:42:43 -0600

    Some Common Styling
    created some common styling to start with, as well a list of colors with some explanation included

--- front-end/nom-wrangler/src/App.js
+++ front-end/nom-wrangler/src/App.js
@@ -14,27 +14,6 @@ function App() {
     <Router>
       <div>
         <Nav/>
-        {/* <nav>
-          <div className='nav'>
-            <Link to="/">
-            <img alt='logo' className='logo' src='../images/nomWrangler2.png'></img>
-            </Link>
-            <div className='buttons'>
-              <Link to="/">
-                <button className="navbutton">HOME</button>
-              </Link>
-              <Link to="/map">
-                <button className="navbutton">MAP</button>
-              </Link>
-              <Link to="/profile">
-                <button className="navbutton">PROFILE</button>
-              </Link>
-              <Link to="/login">
-                <button className="navbutton">LOGIN</button>
-              </Link>
-            </div>
-          </div>
-        </nav> */}
 
         {/* Define Routes */}
         <Routes>
--- front-end/nom-wrangler/src/component/home/home.js
+++ front-end/nom-wrangler/src/component/home/home.js
@@ -3,18 +3,37 @@ import './home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='header'> FEATURED </h1>
+    <div className='home'>
+      <h1 className='mainheader'> FEATURED </h1>
 
-      <div className='rest'>
-        <div className='singleRest'> 
-            <div>
-              <h1>Lucky J Steakhouse & Arena</h1>
-            </div>
+      <div className='restO'>
+        <div className='singleRest'>
+          <img className='restImg' src='../images/11steakhouse.jpg' />
+          <div className='restDetails'>
+            <h1>Lucky J Steakhouse & Arena</h1>
+            <h2 className='restAddress'>11664 Fir Rd, Carthage, MO 64836</h2>
+            <p>Lucky J Steakhouse & Arena opened its doors in November 2001...</p>
+          </div>
         </div>
       </div>
-      <h1 className='header'> TOP REVIEWERS </h1>
 
+      <br></br>
+      <div className='restB'>
+        <div className='singleRest'>
+          <div className='restDetails'>
+            <h1 >Lucky J Steakhouse & Arena</h1>
+            <h2 className='restAddress'>11664 Fir Rd, Carthage, MO 64836</h2>
+            <p> Lucky J Steakhouse & Arena opened its doors in November 2001 in Carthage, Missouri, and has since become one of the most popular local restaurants as well as the premier equine arena in the four-state area. As our slogan says, whether you’re looking for great food or good times, Lucky J is the place to be! </p>
+          </div>
+          <img className='restImg' src='../images/11steakhouse.jpg'></img>
+        </div>
+      </div>
+      <div>
+        <button className='BlueButton'>FIND MORE RESTAURANTS</button>
+      </div>
+      <h1 className='mainheader'> TOP REVIEWERS </h1>
+
+      
     </div>
   )
 }
