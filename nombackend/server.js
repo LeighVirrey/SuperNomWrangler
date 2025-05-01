6b58caa80fed99178942311782c10f09c649a961
TOONZK <126704941+LeighVirrey@users.noreply.github.com>
2025-05-01 11:46:25 -0600

    port change
    we had the same port for front end and back end

--- nombackend/server.js
+++ nombackend/server.js
@@ -551,7 +551,7 @@ app.get('/api/restaurants', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
