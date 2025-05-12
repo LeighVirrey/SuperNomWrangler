3e0e68143c7322c919f80ba3ad9e9cf226398034
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2025-05-12 17:32:26 -0600

    connection to the backend

--- nombackend/server.js
+++ nombackend/server.js
@@ -969,7 +969,7 @@ app.get('/api/restaurants', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
