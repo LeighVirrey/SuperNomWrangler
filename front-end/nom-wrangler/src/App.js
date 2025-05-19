dab235a97debc3da15d22477bf4ee1d13761bbf8
Astokes2604 <129238930+Astokes2604@users.noreply.github.com>
2025-05-19 11:18:30 -0600

    changed data to be hard coded for the time being

--- front-end/nom-wrangler/src/App.js
+++ front-end/nom-wrangler/src/App.js
@@ -7,7 +7,7 @@ import Login from './component/login/login'
 import Nav from './component/nav/nav'
 import About from './component/about/about'
 import Review from './component/userReview/UserReview'
-
+import Details from './component/restaurantDetails/RestaurantDetails'
 
 
 function App() {
@@ -25,6 +25,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/map" element={<Map />} />
           <Route path="/review" element={<Review />} />
+          <Route path="/restaurantDetails" element={<Details />} />
         </Routes>
       </div>
     </Router>
--- front-end/nom-wrangler/src/component/restaurantDetails/RestaurantDetails.js
+++ front-end/nom-wrangler/src/component/restaurantDetails/RestaurantDetails.js
@@ -8,14 +8,47 @@ function RestaurantDetails() {
     const [restaurant, setRestaurant] = useState(null);
 
     useEffect(() => {
-        fetch(`http://localhost:3000/api/restaurants/${id}`)
-            .then((response) => response.json())
-            .then((data) => setRestaurant(data))
-            .catch((error) => console.error("Error fetching restaurant:", error));
+        // Hardcoded demo data
+        const demoData = {
+            _id: id,
+            name: "The Gourmet Kitchen",
+            image: "/images/11steakhouse.jpg",
+            rating: 4.6,
+            price: "$25 - $49.99",
+            style: "Modern European",
+            type: "Fine Dining",
+            address: "123 Flavor Street, Foodie Town",
+            description: "An exquisite dining experience blending modern techniques with classic flavors.",
+            reviews: [
+                {
+                    _id: "r1",
+                    rating: 5,
+                    description: "Amazing food and great atmosphere!",
+                    user: {
+                        username: "foodlover92",
+                        profileImage: "/images/hotdog.png"
+                    }
+                },
+                {
+                    _id: "r2",
+                    rating: 4,
+                    description: "Loved the desserts. Would visit again.",
+                    user: {
+                        username: "gourmetfan",
+                        profileImage: "/images/steak.jpg"
+                    }
+                }
+            ]
+        };
+
+        // Simulate API delay for better UX
+        setTimeout(() => {
+            setRestaurant(demoData);
+        }, 500);
     }, [id]);
 
     const handleLeaveReview = () => {
-        navigate(`/leave-review/${id}`);
+        navigate(`/review`);
     };
 
     if (!restaurant) {
