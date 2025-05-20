900054266583ac57c4001426e07c669c0e77e2d0
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2025-05-20 11:36:08 -0600

    rawr

--- front-end/nom-wrangler/src/component/login/login.js
+++ front-end/nom-wrangler/src/component/login/login.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './login.css';
 
-const apiUrl = 'http://localhost:6000/login';
+const apiUrl = 'http://localhost:4000/login';
 
 const Login = () => {
   const [loginData, setLoginData] = useState({ 
