25839a40387d66be43b59cd89101d3d334f02fa8
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2025-05-27 11:30:42 -0600

    btn linked to editprofile page

--- front-end/nom-wrangler/src/component/profile/profile.js
+++ front-end/nom-wrangler/src/component/profile/profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './profile.css';
 
 const Profile = () => {
@@ -26,7 +27,9 @@ const Profile = () => {
                     </div>
                     <div className="gear-icon">⚙️</div>
                 </div>
+                <Link to="/EditProfile" className="edit-profile-link">
                 <button className="edit-profile-btn">Edit Profile</button>
+                </Link>
             </div>
 
             <div className="reviews-section">
